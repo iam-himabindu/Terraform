924f15db77c34d600b734ff58b4701a1c915deaf
iam-himabindu <himalearnings88@gmail.com>
2026-01-01 10:07:51 +0530

    tfvar file practice day-2

---
+++ Day-2-tfvars/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    description = "Passing values to main.tf"
+    type = string
+    default = ""
+  
+}
+
+variable "type" {
+    description = "Passing values to main.tf"
+    type = string
+    default = ""
+  
+}
