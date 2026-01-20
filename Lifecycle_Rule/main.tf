5c8d597b5f28648585666b529c322a7b461b0a6d
iam-himabindu <himalearnings88@gmail.com>
2026-01-20 19:16:19 +0530

    folder namings

--- Lifecycle_Rule/main.tf
+++ Lifecycle_Rule/main.tf
@@ -3,12 +3,12 @@ resource "aws_instance" "name" {
     instance_type = "t2.micro"
     availability_zone = "us-east-1c"
     tags = {
-      Name = "Lifecycle-3"
+      Name = "Lifecycle-1"
     }
   
 
 lifecycle {
-      #prevent_destroy = true               # this attribute will create the new object first and then destroy the old one
+      #prevent_destroy = true     # this attribute will create the new object first and then destroy the old one
       create_before_destroy = true        #Terraform will error when it attempts to destroy a resource when this is set to true
       #ignore_changes = [ tags,instance_type,]      # This attribute will never update the object but will be able to create or destroy it.
 
--- Version_locking/main.tf
+++ Version_locking/main.tf
@@ -2,3 +2,5 @@ resource "aws_instance" "name" {
    ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.micro"
 }
+
+###### we can lock the version ############
